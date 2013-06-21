2d8f75d93c58bc8a69b59831143ed0c7dba586dd
aczire <dev@aczire.com>
2013-06-21 23:04:00 +0530

    Copyright notice

--- code/sys/ndisbind.c
+++ code/sys/ndisbind.c
@@ -1,10 +1,30 @@
 /*
-
-    NDIS protocol entry points and utility routines to handle binding
-    and unbinding from adapters.
-
+* Copyright 2013, New Tribes Mission Inc, (ntm.org)
+*
+* All rights reserved.
+* 
+* This file is part of WinTAP. WinTAP is released under 
+* GNU General Public License, version 2.
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* 
 */
 
+/*
+    NDIS protocol entry points and utility routines to handle binding
+    and unbinding from adapters.
+*/
 
 #include "precomp.h"
 
